42b5b31fffdc902d1f5c2629675afbfac9bfd325
chengshuyi.csy <chengshuyi@linux.alibaba.com>
2022-06-14 17:00:24 +0800

    add ringbuffer kernel module
    Signed-off-by: Wenan Mao <wenan.mao@linux.alibaba.com>

--- lcc/glcc/README.md
+++ lcc/glcc/README.md
@@ -15,6 +15,16 @@
 
 4. 运行命令 `make ebpfdrv`
 
+#### ringbuffer 驱动编译流程
+
+1. 安装目标内核版本的 kernel-devel 包，以 3.10.0-327.ali2017.alios7.x86_64 为例： `yum install kernel-devel-3.10.0-327.ali2017.alios7.x86_64`
+
+2. 运行命令 `mkdir build & cd build` 创建编译目录
+
+3. 运行命令 `cmake ..` 生成 Makefile 文件
+
+4. 运行命令 `make ringbuffer`
+
 #### hook 库编译流程
 
 1. 运行命令 `mkdir build & cd build` 创建编译目录
---
+++ lcc/glcc/lib/ringbuffer/main.c
@@ -0,0 +1,70 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/version.h>
+#include <linux/kernel.h>
+#include <linux/kallsyms.h>
+
+
+typedef void (*register_module_helper_t)(struct bpf_func_proto *helper);
+typedef void (*register_module_map_t)(struct bpf_map_ops *map);
+typedef void (*unregister_module_helper_t)(struct bpf_func_proto *helper);
+typedef void (*unregister_module_map_t)(struct bpf_map_ops *map);
+
+register_module_helper_t register_module_helper_p = NULL;
+unregister_module_helper_t unregister_module_helper_p = NULL;
+register_module_map_t register_module_map_p = NULL;
+unregister_module_map_t unregister_module_map_p = NULL;
+
+extern struct bpf_map_ops ringbuf_map_ops;
+extern struct bpf_func_proto bpf_ringbuf_reserve_proto;
+extern struct bpf_func_proto bpf_ringbuf_submit_proto;
+extern struct bpf_func_proto bpf_ringbuf_discard_proto;
+extern struct bpf_func_proto bpf_ringbuf_output_proto;
+extern struct bpf_func_proto bpf_ringbuf_query_proto;
+
+static int ringbuffer_init(void)
+{
+    register_module_helper_p = (register_module_helper_t)kallsyms_lookup_name("register_module_helper");
+    if (register_module_helper_p == NULL)
+        goto fail_find;
+
+    unregister_module_helper_p = (register_module_helper_t)kallsyms_lookup_name("unregister_module_helper");
+    if (unregister_module_helper_p == NULL)
+        goto fail_find;
+    
+    register_module_map_p = (register_module_map_t)kallsyms_lookup_name("register_module_map");
+    if (register_module_map_p == NULL)
+        goto fail_find;
+    
+    unregister_module_map_p = (register_module_map_t)kallsyms_lookup_name("unregister_module_map");
+    if (unregister_module_map_p == NULL)
+        goto fail_find;
+    
+
+    register_module_map_p(&ringbuf_map_ops);
+    register_module_helper_p(&bpf_ringbuf_reserve_proto);
+    register_module_helper_p(&bpf_ringbuf_submit_proto);
+    register_module_helper_p(&bpf_ringbuf_discard_proto);
+    register_module_helper_p(&bpf_ringbuf_output_proto);
+    register_module_helper_p(&bpf_ringbuf_query_proto);
+	return 0;
+fail_find:
+    printk(KERN_NOTICE "failed to find kernel symbols\n");
+	return -1;
+}
+void ringbuffer_exit(void)
+{
+    if (unregister_module_map_p)
+        unregister_module_map_p(&ringbuf_map_ops);
+    if (unregister_module_helper_p)
+    {
+        unregister_module_helper_p(&bpf_ringbuf_reserve_proto);
+        unregister_module_helper_p(&bpf_ringbuf_submit_proto);
+        unregister_module_helper_p(&bpf_ringbuf_discard_proto);
+        unregister_module_helper_p(&bpf_ringbuf_output_proto);
+        unregister_module_helper_p(&bpf_ringbuf_query_proto);
+    }
+}
+
+module_init(ringbuffer_init);
+module_exit(ringbuffer_exit);
