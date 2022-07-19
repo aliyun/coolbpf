d7e95c019c64e69aaa38af7f2e80fcf0f3d15d88
hengkun.zhong <hengkun.zhong@linux.alibaba.com>
2022-07-19 18:52:15 +0800

    golcc: rexample support set custom btf path

--- lcc/golcc/rexample/hello/hello.go
+++ lcc/golcc/rexample/hello/hello.go
@@ -3,21 +3,44 @@ package main
 import "C"
 
 import (
+	"flag"
 	"fmt"
-	"github.com/aquasecurity/libbpfgo"
 	"os"
+	"strings"
 	"time"
+
+	"github.com/aquasecurity/libbpfgo"
+	"golang.org/x/sys/unix"
 )
+
+var (
+	h    bool
+	path string
+)
+
+func init() {
+	var name unix.Utsname
+	unix.Uname(&name)
+	defaultPath := fmt.Sprintf("/boot/vmlinux-%s", name.Release)
+	defaultPath = strings.Trim(defaultPath, "\x00")
+
+	flag.BoolVar(&h, "h", false, "this help")
+	flag.StringVar(&path, "p", defaultPath, "set BTF custom path")
+}
 
 func main() {
 
-	bpfModule, err := libbpfgo.NewModuleFromFile("hello.bpf.o")
+	flag.Parse()
+	fmt.Println("btf path:", path)
+	bpfModule, err := libbpfgo.NewModuleFromFileArgs(libbpfgo.NewModuleArgs{
+		BPFObjPath: "hello.bpf.o",
+		BTFObjPath: path,
+	})
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(-1)
 	}
 	defer bpfModule.Close()
-
 	err = bpfModule.BPFLoadObject()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
