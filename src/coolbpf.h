6efee262a5595ce52778c92217c71b8308c739d0
Shuyi Cheng <chengshuyi@linux.alibaba.com>
2022-12-22 15:13:43 +0800

    provide all in one header and easier to create map
    Signed-off-by: Shuyi Cheng <chengshuyi@linux.alibaba.com>

--- src/coolbpf.h
+++ src/coolbpf.h
@@ -4,11 +4,51 @@
 
 #ifdef __VMLINUX_H__
 
+#include <vmlinux.h>
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_endian.h>
 
+#define ns() bpf_ktime_get_ns()
+#define pid() (bpf_get_current_pid_tgid() >> 32)
+#define tid() ((u32)bpf_get_current_pid_tgid())
+#define comm(c) bpf_get_current_comm(c, sizeof(c))
+// return u32
+#define cpu() bpf_get_smp_processor_id()
+
+#define BPF_MAP(_name, _type, _key_type, _value_type, _max_entries) \
+    struct                                                          \
+    {                                                               \
+        __uint(type, _type);                                        \
+        __uint(max_entries, _max_entries);                          \
+        __type(key, _key_type);                                     \
+        __type(value, _value_type);                                 \
+    } _name SEC(".maps");
+
+#define BPF_HASH(_name, _key_type, _value_type, _max_entries) \
+    BPF_MAP(_name, BPF_MAP_TYPE_HASH, _key_type, _value_type, _max_entries)
+
+#define BPF_LRU_HASH(_name, _key_type, _value_type, _max_entries) \
+    BPF_MAP(_name, BPF_MAP_TYPE_LRU_HASH, _key_type, _value_type, _max_entries)
+
+#define BPF_ARRAY(_name, _value_type, _max_entries) \
+    BPF_MAP(_name, BPF_MAP_TYPE_ARRAY, u32, _value_type, _max_entries)
+
+#define BPF_PERCPU_ARRAY(_name, _value_type, _max_entries) \
+    BPF_MAP(_name, BPF_MAP_TYPE_PERCPU_ARRAY, u32, _value_type, _max_entries)
+
+#define BPF_PROG_ARRAY(_name, _max_entries) \
+    BPF_MAP(_name, BPF_MAP_TYPE_PROG_ARRAY, u32, u32, _max_entries)
+
+#define BPF_PERF_OUTPUT(_name, _max_entries) \
+    BPF_MAP(_name, BPF_MAP_TYPE_PERF_EVENT_ARRAY, int, __u32, _max_entries)
+
+typedef __u64 stack_trace_t[MAX_STACK_DEPTH];
+#define BPF_STACK_TRACE(_name, _max_entries) \
+    BPF_MAP(_name, BPF_MAP_TYPE_STACK_TRACE, u32, stack_trace_t, _max_entries)
 
 #else
-
-
 
 #define COOLBPF_MAJOR_VERSION 0
 #define COOLBPF_MINOR_VERSION 1
