0eeb1fb2d7f6b91f871dbb6ec74f2e1fcd1c03f9
Shuyi Cheng <chengshuyi@linux.alibaba.com>
2022-12-22 13:13:59 +0800

    build.sh: use absolute path
    Signed-off-by: Shuyi Cheng <chengshuyi@linux.alibaba.com>

--- build.sh
+++ build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
 
+
+root=$(cd "$(dirname "$0")";pwd)
+
 if [ -z $OBJDIR ]; then
-    OBJDIR=$(pwd)/build/coolbpf
+    OBJDIR=${root}/build/coolbpf
 fi
 
 if [ -z $DESTDIR ]; then
-    DESTDIR=$(pwd)/build 
+    DESTDIR=${root}/build 
 fi
 
 
-make -C third/libbpf/src OBJDIR=${OBJDIR} DESTDIR=${DESTDIR} INCLUDEDIR= LIBDIR= UAPIDIR= install
-make -C src/ OBJDIR=${OBJDIR} DESTDIR=${DESTDIR} INCLUDEDIR= LIBDIR= UAPIDIR= install
+make -C ${root}/third/libbpf/src OBJDIR=${OBJDIR} DESTDIR=${DESTDIR} INCLUDEDIR= LIBDIR= UAPIDIR= install
+make -C ${root}/src OBJDIR=${OBJDIR} DESTDIR=${DESTDIR} INCLUDEDIR= LIBDIR= UAPIDIR= install
