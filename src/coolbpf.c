827d3f74551c7e275dca3aa364ca4fbb2de6a983
Shuyi Cheng <chengshuyi@linux.alibaba.com>
2022-12-26 10:59:13 +0800

    add api to create perf thread
    Signed-off-by: Shuyi Cheng <chengshuyi@linux.alibaba.com>

--- src/coolbpf.c
+++ src/coolbpf.c
@@ -1,12 +1,12 @@
 /**
  * @file coolbpf.c
  * @author Shuyi Cheng (chengshuyi@linux.alibaba.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-12-22
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
 #ifdef __VMLINUX_H__
@@ -22,6 +22,7 @@
 #include <bpf/libbpf.h>
 #include <bpf/bpf.h>
 #include <asm/unistd.h>
+#include <pthread.h>
 
 #include "coolbpf.h"
 uint32_t coolbpf_major_version(void)
@@ -41,6 +42,51 @@ const char *coolbpf_version_string(void)
     return "v" _S(COOLBPF_MAJOR_VERSION) "." _S(COOLBPF_MINOR_VERSION);
 #undef _S
 #undef __S
+}
+
+void *perf_thread_worker(void *ctx)
+{
+    int err;
+    struct perf_buffer *pb = NULL;
+    struct perf_buffer_opts pb_opts = {};
+    struct perf_thread_arguments *args = (struct perf_thread_arguments *)ctx;
+    int timeout_ms = args->timeout_ms == 0 ? 100 : args->timeout_ms;
+
+    pb_opts.sample_cb = args->sample_cb;
+    pb = perf_buffer__new(args->mapfd, args->pg_cnt == 0 ? 128 : args->pg_cnt, &pb_opts);
+    if (!pb)
+    {
+        err = -errno;
+        fprintf(stderr, "failed to open perf buffer: %d\n", err);
+        return NULL;
+    }
+
+    while (true)
+    {
+        err = perf_buffer__poll(pb, timeout_ms);
+        if (err < 0 && err != -EINTR)
+        {
+            fprintf(stderr, "error polling perf buffer: %s\n", strerror(-err));
+            goto cleanup;
+        }
+        /* reset err to return 0 if exiting */
+        err = 0;
+    }
+cleanup:
+    perf_buffer__free(pb);
+    return NULL;
+}
+
+pthread_t initial_perf_thread(struct perf_thread_arguments *args)
+{
+    pthread_t thread;
+    pthread_create(&thread, NULL, perf_thread_worker, args);
+    return thread;
+}
+
+int kill_perf_thread(pthread_t thread)
+{
+    return pthread_cancel(thread);
 }
 
 #endif
--- src/coolbpf.h
+++ src/coolbpf.h
@@ -101,6 +101,19 @@ typedef __u64 stack_trace_t[MAX_STACK_DEPTH];
 #define COOLBPF_MAJOR_VERSION 0
 #define COOLBPF_MINOR_VERSION 1
 
+
+/**
+ * @brief Parameters required to create perf threads
+ * 
+ */
+struct perf_thread_arguments {
+    int mapfd;                          /**< fd of perf event map */
+    perf_buffer_sample_fn sample_cb;    /**< callback function */
+    perf_buffer_lost_fn lost_cb;        /**< callback function when the event is lost */
+    int pg_cnt;                         /**< perf buffer size in page, default is 128 page */
+    int timeout_ms;                     /**< timeout of perf poll in ms, default is 100ms */
+};
+
 /**
  * @brief get coolbpf major verison
  * 
@@ -122,7 +135,21 @@ uint32_t coolbpf_minor_version();
  */
 const char *coolbpf_version_string(void);
 
+/**
+ * @brief Create a perf thread to receive perf events
+ * 
+ * @param mapfd fd of map of type BPF_MAP_TYPE_PERF_EVENT_ARRAY
+ * @return pthread_t thread id, you can use it later to destroy the thread
+ */
+pthread_t initial_perf_thread(struct perf_thread_arguments *args);
 
+/**
+ * @brief Destroy perf thread
+ * 
+ * @param thread perf thread id
+ * @return int 
+ */
+int kill_perf_thread(pthread_t thread);
 
 #endif
 
