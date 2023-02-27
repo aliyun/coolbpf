5ab47f35888ecc440732f3c30c0c7182a8c39a1e
Shuyi Cheng <chengshuyi@linux.alibaba.com>
2023-02-27 09:08:15 +0800

    lwcb: remove .env
    Signed-off-by: Shuyi Cheng <chengshuyi@linux.alibaba.com>

--- lwcb/.env/lib/python3.6/site-packages/setuptools_rust/__init__.py
+++
@@ -1,6 +0,0 @@
-from .build import build_rust
-from .clean import clean_rust
-from .extension import Binding, RustExtension, Strip
-from .version import version as __version__
-
-__all__ = ("Binding", "RustExtension", "Strip", "build_rust", "clean_rust")
