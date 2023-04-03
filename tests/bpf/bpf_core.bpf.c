7a20bfa2d2731cf499a2e8841879aae8de581bed
Shuyi Cheng <chengshuyi@linux.alibaba.com>
2023-04-03 13:37:59 +0800

    add test for bpf_core
    Signed-off-by: Shuyi Cheng <chengshuyi@linux.alibaba.com>

---
+++ tests/bpf/bpf_core.bpf.c
@@ -0,0 +1,15 @@
+
+
+
+
+#include "vmlinux.h"
+#include <coolbpf/coolbpf.h>
+
+
+SEC("kprobe/tcp_sendmsg")
+int BPF_KPROBE(tcp_sendmsg, u64 sk) {
+    // doesn't matter if parameter is 'struct task_struct *' type, we just check if compilation is ok.
+    bpf_core_task_struct_state(sk);
+    return 0;
+}
+
---
+++ tests/bpf_core.c
@@ -0,0 +1,11 @@
+#include <coolbpf/coolbpf.h>
+#include <criterion/criterion.h>
+#include <coolbpf/log.h>
+#include "bpf_core.skel.h"
+
+Test(bpf, bpf_core_task_struct_state)
+{
+    bump_memlock_rlimit();
+    struct coolbpf_object  *cb = coolbpf_object_new(bpf_core);
+    cr_assert_not_null(cb);
+}
