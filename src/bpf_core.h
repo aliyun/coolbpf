edd2de1a27c960f681c5438f094e91736414cd28
Shuyi Cheng <chengshuyi@linux.alibaba.com>
2023-04-03 15:34:38 +0800

    bpf_core: get state size, addr
    Signed-off-by: Shuyi Cheng <chengshuyi@linux.alibaba.com>

--- src/bpf_core.h
+++ src/bpf_core.h
@@ -3,6 +3,11 @@
 #ifndef __BPF_CORE_H
 #define __BPF_CORE_H
 
+#ifndef bpf_core_field_offset
+#define bpf_core_field_offset(field...) \
+    __builtin_preserve_field_info(___bpf_field_ref(field), BPF_FIELD_BYTE_OFFSET)
+#endif
+
 struct task_struct_____state
 {
     unsigned int __state;
@@ -12,19 +17,43 @@ struct task_struct___state
     volatile long state;
 };
 
-static __always_inline long bpf_core_task_struct_state(void *task)
+static __always_inline u64 bpf_core_task_struct_state_offset(void *task)
 {
-    long res = 0;
     struct task_struct_____state *__state = task;
     struct task_struct___state *state = task;
     if (bpf_core_field_exists(__state->__state))
     {
-        bpf_core_read(&res, sizeof(__state->__state), &__state->__state);
+        return bpf_core_field_offset(__state->__state);
     }
     else
     {
-        bpf_core_read(&res, sizeof(state->state), &state->state);
+        return bpf_core_field_offset(state->state);
     }
+}
+
+static __always_inline u32 bpf_core_task_struct_state_size(void *task)
+{
+    struct task_struct_____state *__state = task;
+    struct task_struct___state *state = task;
+    if (bpf_core_field_exists(__state->__state))
+    {
+        return bpf_core_field_size(__state->__state);
+    }
+    else
+    {
+        return bpf_core_field_size(state->state);
+    }
+}
+
+static __always_inline u64 bpf_core_task_struct_state_addr(void *task)
+{
+    return bpf_core_task_struct_state_offset(task) + (u64)task;
+}
+
+static __always_inline long bpf_core_task_struct_state(void *task)
+{
+    long res = 0;
+    bpf_probe_read(&res, bpf_core_task_struct_state_size(task), bpf_core_task_struct_state_addr(task));
     return res;
 }
 
