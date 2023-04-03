47b179ba1b81a3f1c2be8aa4860e9632a0a77691
Shuyi Cheng <chengshuyi@linux.alibaba.com>
2023-04-03 15:47:12 +0800

    comment bpf_core_task_struct_state
    Signed-off-by: Shuyi Cheng <chengshuyi@linux.alibaba.com>

--- src/bpf_core.h
+++ src/bpf_core.h
@@ -3,6 +3,8 @@
 #ifndef __BPF_CORE_H
 #define __BPF_CORE_H
 
+#if 0
+
 #ifndef bpf_core_field_offset
 #define bpf_core_field_offset(field...) \
     __builtin_preserve_field_info(___bpf_field_ref(field), BPF_FIELD_BYTE_OFFSET)
@@ -23,11 +25,11 @@ static __always_inline u64 bpf_core_task_struct_state_offset(void *task)
     struct task_struct___state *state = task;
     if (bpf_core_field_exists(__state->__state))
     {
-        return bpf_core_field_offset(__state->__state);
+        return bpf_core_field_offset(struct task_struct_____state, __state);
     }
     else
     {
-        return bpf_core_field_offset(state->state);
+        return bpf_core_field_offset(struct task_struct___state, state);
     }
 }
 
@@ -55,6 +57,12 @@ static __always_inline long bpf_core_task_struct_state(void *task)
     long res = 0;
     bpf_probe_read(&res, bpf_core_task_struct_state_size(task), bpf_core_task_struct_state_addr(task));
     return res;
+}
+#endif 
+
+static __always_inline bool bpf_core_task_struct_thread_info_exist(struct task_struct *task)
+{
+    return bpf_core_field_exists(task->thread_info);
 }
 
 #endif
