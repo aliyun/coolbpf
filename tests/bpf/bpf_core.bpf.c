a44ff91f9c8a74bbbfaae2626d588395cb4499b1
Shuyi Cheng <chengshuyi@linux.alibaba.com>
2024-02-19 14:00:16 +0800

    fix test compilation error
    Signed-off-by: Shuyi Cheng <chengshuyi@linux.alibaba.com>

--- tests/bpf/bpf_core.bpf.c
+++ tests/bpf/bpf_core.bpf.c
@@ -7,7 +7,7 @@
 
 
 SEC("kprobe/tcp_sendmsg")
-int BPF_KPROBE(tcp_sendmsg, u64 sk) {
+int BPF_KPROBE(tcp_sendmsg, struct sock *sk) {
     // doesn't matter if parameter is 'struct task_struct *' type, we just check if compilation is ok.
     bpf_core_task_struct_thread_info_exist(sk);
     return 0;
