c11d6bde919d2d3261db648545e985c1785e694b
Shuyi Cheng <chengshuyi@linux.alibaba.com>
2024-06-25 18:42:33 +0800

    support compiling bpf program in arm64 platform
    Signed-off-by: Shuyi Cheng <chengshuyi@linux.alibaba.com>

--- scripts/cmake/arch.cmake
+++ scripts/cmake/arch.cmake
@@ -1,3 +1,7 @@
 if(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "x86_64")
     set(ARCH "x86")
+elseif(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "aarch64")
+    set(ARCH "arm64")
+else()
+    set(ARCH "unknown")
 endif()
