9ebc4204eb13459686f4b7a06ba4191e5978e89e
Shuyi Cheng <chengshuyi@linux.alibaba.com>
2025-01-09 13:46:30 +0800

    profiler: handle so without program headers
    Signed-off-by: Shuyi Cheng <chengshuyi@linux.alibaba.com>

--- src/profiler/src/profiler.rs
+++ src/profiler/src/profiler.rs
@@ -217,7 +217,13 @@ impl<'a> Profiler<'a> {
                 }
             };
 
-            let va = info.file_offset_to_virtual_address(map.offset).unwrap();
+            let va = match info.file_offset_to_virtual_address(map.offset) {
+                Some(x) => x,
+                None => {
+                    log::warn!("executable program headers not found, skip it");
+                    continue;
+                }
+            };
             let bias = map.start - va;
             let exe = match self
                 .executables
